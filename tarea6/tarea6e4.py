c0d89c0fd008675082684c9b07627611c7541510
mikeacb <ex.bex@hotmail.com>
2021-10-06 12:14:29 -0500

    Terminada la tarea6e4

---
+++ tarea6/tarea6e4.py
@@ -0,0 +1,16 @@
+# Hacer un programa que realice una suma y
+# le pregunte al usuario si quiere seguir sumando números,
+# si la respuesta es no,
+# imprimir el resultado de todos los números sumados
+
+print("si para continuar, no para salir")
+ans = input("¿Desea continuar? ... ")
+temp = 0
+while (ans == "si"):
+    if (ans == "si"):
+        num1 = int(input("Número 1: "))
+        num2 = int(input("Número 2: "))
+        suma = num1 + num2
+        temp += suma
+        ans = input("¿Desea continuar? ... ")
+print(f"Suma total: {temp}")
