f4c32168401408e17c4ef9deb90552e2ad1bcaf7
mikeacb <ex.bex@hotmail.com>
2021-10-06 16:23:11 -0500

    Terminada la tarea7e4 (pero con dudas), por ende, terminadas los ejercicios de la clase 7

---
+++ tarea7/tarea7e4.py
@@ -0,0 +1,14 @@
+# Calcular en una función el cambio de moneda de pesos a dólares,
+# ese valor mandarlo al código llamador y
+# hacer la conversión de dólares a euros,
+# Imprimir los pesos, dólares y euros.
+
+def pesosADolares(moneda):
+    return moneda * 0.048
+
+cambio = float(input("¿Cuántos pesos desea convertir? "))
+llamada = pesosADolares(cambio)
+euros = llamada * 0.87
+print(f"Pesos: {cambio}\n")
+print(f"Dólares: {llamada}\n")
+print(f"Euros: {euros}\n")
