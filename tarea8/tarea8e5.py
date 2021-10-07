4c19782c0da6396f0e2e861e764cb3436c6545cc
mikeacb <ex.bex@hotmail.com>
2021-10-06 20:26:11 -0500

    Terminada la tarea8, pero falta revisar la tarea5e3-5e4

--- tarea8/tarea8e5.py
+++ tarea8/tarea8e5.py
@@ -1,2 +1,13 @@
 # Investigar algún modulo en internet y utilizarlo en un programa
 
+from colorama import Fore, Style
+
+# init(convert= True)
+
+metros = float(input("Dame tu altura: "))
+if (metros <= 1.50):
+    print(Fore.RED + "Persona de baja estatura")
+elif (metros > 1.50) and (metros <= 1.70):
+    print(Fore.YELLOW + "Persona de media estatura")
+else:
+    print(Fore.GREEN + "Persona alta")
